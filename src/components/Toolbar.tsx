b0250fed0b61801650931af02cac647617ba9c00
João Lucas de Oliveira <joaolucas.deoliveira56@gmail.com>
2023-03-21 12:32:11 -0300

    feat: add mui and menu toolbar

---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+export const ToolbarMenu = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  return (
+    <AppBar component="nav">
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+        >
+          MUI
+        </Typography>
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <Button sx={{ color: "#fff" }}>Adicionar</Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
